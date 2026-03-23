f172a4ede792b173db204ffbfcd1a6eed71b5177
oideyasu9-prog <oideyasu9@gmail.com>
2026-03-23 10:05:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 
 app = Flask(__name__)
 
-OPENAI_API_KEY = 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
-LINE_ACCESS_TOKEN =TFeUUPncqDT2I2+JOwra4mspAsYSjW37S+cdKFjxetvL2rFb5tWBB7hp5hpSgqLMscXl0JHGu2aZgZywHZ6RI2DTac3DO4d9n/mTpDv4zFNDp4AzUN2d+TFoEhYCLBz/WbKhj2/jt8+toBXm2YW9wwdB04t89/1O/w1cDnyilFU=
+OPENAI_API_KEY = os.environ.get("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")
+LINE_ACCESS_TOKEN = os.environ.get(TFeUUPncqDT2I2+JOwra4mspAsYSjW37S+cdKFjxetvL2rFb5tWBB7hp5hpSgqLMscXl0JHGu2aZgZywHZ6RI2DTac3DO4d9n/mTpDv4zFNDp4AzUN2d+TFoEhYCLBz/WbKhj2/jt8+toBXm2YW9wwdB04t89/1O/w1cDnyilFU="")
 
 @app.route("/")
 def home():
@@ -18,7 +18,6 @@ def webhook():
     user_message = body["events"][0]["message"]["text"]
     reply_token = body["events"][0]["replyToken"]
 
-    # OpenAI
     res = requests.post(
         "https://api.openai.com/v1/responses",
         headers={
@@ -33,7 +32,6 @@ def webhook():
 
     ai_reply = res.json()["output"][0]["content"][0]["text"]
 
-    # LINE返信
     requests.post(
         "https://api.line.me/v2/bot/message/reply",
         headers={
